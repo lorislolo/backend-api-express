98b8854b72f9e7d3b5bbfc55c84bb90f07ce0ea2
lorislolo <lorena.macedo@aluno.ifsp.edu.br>
2024-02-09 16:25:48 -0300

    inicio ao express

--- src/server.js
+++ src/server.js
@@ -1,1 +1,18 @@
-console.log("Olá, mundooo!");
+// const express = require('express')
+import express from 'express'
+import {PORT, HOST} from './config.js'
+
+const app = express()
+
+
+app.get('/', (req, res) => {
+  res.json('Hello World!')
+})
+
+app.post('/produto', (req,res) =>{
+  res.json({message: 'Hello Word POST!'})
+})
+
+app.listen(PORT, () => {
+  console.log(`Server running on ${HOST}:${PORT} `)
+})
